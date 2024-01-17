f4c0989e987bdcc1ca59e24482201f6f32685699
MrZnn <15210602359@139.com>
2024-01-17 15:25:29 +0800

    change deploy

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { set_sidebar } from "./utils/auto-gen-sidebar.mjs";	// 改成自己的�
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/docs-demo/',
   head:[["link", {rel: "icon", href: "/logo.png"}]],
   title: "My Awesome Project",
   description: "A VitePress Site",
